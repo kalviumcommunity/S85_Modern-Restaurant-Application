a889bc2aa9df84a64e6227b5a40d5978331567e9
Subhadeep-Samanta <subhadeepsamanta1535@gmail.com>
2025-02-21 09:25:11 +0530

    MongoDB Client Connected

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './database/db.js'; // Import database connection
 
-app.get("/ping", (req, res) => {
-  res.send("pong");
+dotenv.config({ path: './config/.env' });
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
+
+// Home Route with Database Status
+app.get('/', (req, res) => {
+  res.send('Welcome to the Restaurant App!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(` Server running on port http://localhost:${PORT}`);
 });
